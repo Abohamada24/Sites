e9c61f9cd252d4bc60a6beaff79c6ecd19b20b01
Abohamada24 <119078550+Abohamada24@users.noreply.github.com>
2026-03-04 22:17:35 +0200

    Create colab_setup.py for Google Colab dependencies

---
+++ colab_setup.py
@@ -0,0 +1,3 @@
+# colab_setup.py
+\n# Install dependencies for Google Colab
+!pip install numpy pandas matplotlib seaborn scikit-learn\n!pip install tensorflow torch  # For machine learning and deep learning\n!pip install beautifulsoup4 requests  # For web scraping
